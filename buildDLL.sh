12a263eaede60ea421167e29fe42a59c882a96e5
Vaino Karppi <vaino.karppi@outlook.com>
2023-04-04 23:28:24 +0300

    Added some colors to buildDLL.sh

--- buildDLL.sh
+++ buildDLL.sh
@@ -5,8 +5,8 @@ startPath=$(pwd)
 date=$(date +%F_%H-%M-%S)
 buildPath=$".//releases//$date//server-client-framework.$version"
 
-echo version:$version
-echo buildPath:$buildPath
+echo -e '\033[32mversion:'$version' \033[1;32m\033[0m'
+echo -e '\033[32mbuildPath:'$buildPath' \033[1;32m\033[0m'
 
 if [ -d "$buildPath" ]; then
     rm -r $buildPath/
@@ -15,6 +15,7 @@ fi
 mkdir -p $buildPath
 cd $buildPath
 
+echo "";echo -e '\033[32mBuilding the template...\033[1;32m\033[0m';echo "";
 dotnet new classlib
 
 cp -R ..//..//..//client//files client
@@ -30,7 +31,7 @@ cd ..
 rm Class1.cs
 
 csProjFile="server-client-framework.$version.csproj"
-echo $csProjFile
+
 sed -i '/<PropertyGroup>/,/<\/PropertyGroup>/ s/<\/PropertyGroup>/  <AssemblyVersion>'$version'<\/AssemblyVersion>\n<\/PropertyGroup>/' $csProjFile
 sed -i '/<PropertyGroup>/,/<\/PropertyGroup>/ s/<\/PropertyGroup>/    <Version>'$version'<\/Version>\n<\/PropertyGroup>/' $csProjFile
 sed -i '/<PropertyGroup>/,/<\/PropertyGroup>/ s/<\/PropertyGroup>/    <FileVersion>'$version'<\/FileVersion>\n<\/PropertyGroup>/' $csProjFile
@@ -39,16 +40,15 @@ sed -i '/<PropertyGroup>/,/<\/PropertyGroup>/ s/<\/PropertyGroup>/    <DebugSymb
 sed -i '/<PropertyGroup>/,/<\/PropertyGroup>/ s/<\/PropertyGroup>/    <DebugType>None<\/DebugType>\n<\/PropertyGroup>/' $csProjFile
 sed -i '/<PropertyGroup>/,/<\/PropertyGroup>/ s/<\/PropertyGroup>/    <NoWarn>8622;1591<\/NoWarn>\n<\/PropertyGroup>/' $csProjFile
 
-
-echo ""; echo "Building shared libary.."; echo ""
+echo "";echo -e '\033[32mBuilding shared libary...\033[1;32m\033[0m';echo "";
 dotnet publish -p:PublishDir=..//shared//,Configuration=Release,AssemblyName=shared-framework.$version
 
-echo ""; echo "Building server only libary.."; echo ""
+echo "";echo -e '\033[32mBuilding server only libary...\033[1;32m\033[0m';echo "";
 rm -r bin
 rm -r client
 dotnet publish -p:PublishDir=..//server//,Configuration=Release,AssemblyName=server-framework.$version
 
-echo ""; echo "Building client only libary.."; echo ""
+echo "";echo -e '\033[32mBuilding client only libary...\033[1;32m\033[0m';echo "";
 rm -r bin
 rm -r server
 cp -R ..//..//..//client//files client
@@ -58,7 +58,5 @@ dotnet publish -p:PublishDir=..//client//,Configuration=Release,AssemblyName=cli
 cd ..
 rm -r "server-client-framework.$version"
 
-echo ""
-echo "BUILD(s) SUCCESS!"
-echo ""
+echo "";echo -e '\033[32mBUILD(s) SUCCESS!\033[1;32m\033[0m';echo "";
 read -p "Press ENTER to exit"
